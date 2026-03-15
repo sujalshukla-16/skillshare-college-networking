20a3b834c1f379d3d085e571bdfdbfb93c5d5b2f
Sujal Shukla <sujalshukla16@gmail.com>
2026-03-15 17:55:34 +0530

    Update internshipRoutes.js

--- backend/routes/internshipRoutes.js
+++ backend/routes/internshipRoutes.js
@@ -1,1 +1,17 @@
+import express from "express";
+import protect from "../middleware/authMiddleware.js";
 
+import {
+  createInternship,
+  getInternships,
+} from "../controllers/internshipController.js";
+
+const router = express.Router();
+
+// Get all internships
+router.get("/", protect, getInternships);
+
+// Add internship (only faculty/alumni allowed inside controller)
+router.post("/", protect, createInternship);
+
+export default router;
