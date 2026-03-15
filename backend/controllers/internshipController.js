90f2bc25c571c474799bab8ee83327a1d95369a5
Sujal Shukla <sujalshukla16@gmail.com>
2026-03-15 17:53:26 +0530

    Update internshipController.js

--- backend/controllers/internshipController.js
+++ backend/controllers/internshipController.js
@@ -1,1 +1,52 @@
+import Internship from "../models/Internship.js";
 
+// ================= CREATE INTERNSHIP =================
+export const createInternship = async (req, res) => {
+  try {
+
+    // Only faculty or alumni can add internships
+    if (req.user.role !== "faculty" && req.user.role !== "alumni") {
+      return res.status(403).json({ message: "Only faculty or alumni can add internships" });
+    }
+
+    const {
+      company,
+      duration,
+      lastDate,
+      stipend,
+      description,
+      applyLink,
+    } = req.body;
+
+    const internship = await Internship.create({
+      postedBy: req.user._id,
+      company,
+      duration,
+      lastDate,
+      stipend,
+      description,
+      applyLink,
+    });
+
+    res.status(201).json(internship);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// ================= GET ALL INTERNSHIPS =================
+export const getInternships = async (req, res) => {
+  try {
+
+    const internships = await Internship.find()
+      .populate("postedBy", "name role")
+      .sort({ createdAt: -1 });
+
+    res.json(internships);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
