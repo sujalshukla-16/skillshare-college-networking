0da035aebc1338a92d3679acaffa4d524f202609
Sujal Shukla <sujalshukla16@gmail.com>
2026-03-15 17:52:04 +0530

    Update courseController.js

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,1 +1,54 @@
+import Course from "../models/Course.js";
 
+// ================= CREATE COURSE =================
+export const createCourse = async (req, res) => {
+  try {
+
+    // Only faculty or alumni can add courses
+    if (req.user.role !== "faculty" && req.user.role !== "alumni") {
+      return res.status(403).json({ message: "Only faculty or alumni can add courses" });
+    }
+
+    const {
+      courseName,
+      domain,
+      duration,
+      eligibleStudents,
+      lastDate,
+      description,
+      applyLink,
+    } = req.body;
+
+    const course = await Course.create({
+      postedBy: req.user._id,
+      courseName,
+      domain,
+      duration,
+      eligibleStudents,
+      lastDate,
+      description,
+      applyLink,
+    });
+
+    res.status(201).json(course);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// ================= GET ALL COURSES =================
+export const getCourses = async (req, res) => {
+  try {
+
+    const courses = await Course.find()
+      .populate("postedBy", "name role")
+      .sort({ createdAt: -1 });
+
+    res.json(courses);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
