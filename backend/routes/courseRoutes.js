25afec54860fc8d896aa20ce6acbed6ee21b5a82
Sujal Shukla <sujalshukla16@gmail.com>
2026-03-15 17:54:32 +0530

    Update courseRoutes.js

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,1 +1,17 @@
+import express from "express";
+import protect from "../middleware/authMiddleware.js";
 
+import {
+  createCourse,
+  getCourses,
+} from "../controllers/courseController.js";
+
+const router = express.Router();
+
+// Get all courses
+router.get("/", protect, getCourses);
+
+// Add new course (only faculty/alumni allowed inside controller)
+router.post("/", protect, createCourse);
+
+export default router;
