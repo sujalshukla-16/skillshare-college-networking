89552e3dad8932a40fc1e8972804869f3b41b8b8
Sujal Shukla <sujalshukla16@gmail.com>
2026-03-27 22:48:49 +0530

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/", protect, sendMessage);
 router.get("/:userId", protect, getMessages);
+router.get("/chat-users", protect, getChatUsers);
 
 export default router;
