759effabd337908c68693acebb315b330c160b9d
Sujal Shukla <sujalshukla16@gmail.com>
2026-03-27 22:54:36 +0530

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
 import { sendMessage, getMessages } from "../controllers/messageController.js";
+import { markAsRead, getUnreadCounts } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.post("/", protect, sendMessage);
 router.get("/:userId", protect, getMessages);
 router.get("/chat-users", protect, getChatUsers);
+router.put("/read/:userId", protect, markAsRead);
+router.get("/unread", protect, getUnreadCounts);
 
 export default router;
