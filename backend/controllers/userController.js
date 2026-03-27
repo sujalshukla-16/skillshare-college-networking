c1c93d8ab7a6745019c1b2b6a7e7ea0e0bb4c9c9
Sujal Shukla <sujalshukla16@gmail.com>
2026-03-27 21:15:43 +0530

    Delete backend/controllers/userController.js

--- backend/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-export const getProfile = async (req, res) => {
-  try {
-    res.json(req.user); // already decoded from token
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
